eb80687ba62f0f2ce72332f260685f07aa98b0ad
hyakumachi <pacanza.berns4884@gmail.com>
2025-04-09 22:04:14 +0800

    5th try

--- capture.py
+++ capture.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-fruit_classes = ['banana', 'chili', 'eggplant', 'ginger', 'lemon']
+fruit_classes = ['Carrot', 'Chili', 'Eggplant', 'Lemon', 'Okra']
 dataset_dir = 'dataset'
 
 os.makedirs(dataset_dir, exist_ok=True)
